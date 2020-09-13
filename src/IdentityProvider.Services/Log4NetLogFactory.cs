767742d091578e4fcb56fb3a363ed72da41be3d7
brunobozic <bruno.bozic@gmail.com>
2020-09-13 23:56:39 +0200

    Added old school style factory for my Log4Net logging provider.

--- src/IdentityProvider.Services/Log4NetLogFactory.cs
+++ src/IdentityProvider.Services/Log4NetLogFactory.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using AutoMapper;
+using IdentityProvider.Infrastructure.ApplicationConfiguration;
+using IdentityProvider.Infrastructure.ApplicationContext;
+using IdentityProvider.Infrastructure.SessionStorageFactories;
+using Logging.WCF.Models.Log4Net;
+using Logging.WCF.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace IdentityProvider.Services
 {
-    class Log4NetLogFactory
+    public class Log4NetLogFactory
     {
+        private static void InitializeLogFactory(bool force)
+        {
+            if (!force)
+            {
+                if (LoggingStorageFactory<ILog4NetLoggingService>.CreateStorageContainer().GetLogger() == null)
+                    CreateLogger();
+            }
+            else
+            {
+                CreateLogger();
+            }
+        }
+
+        public static void StoreLogger(ILog4NetLoggingService loggingService)
+        {
+            if (loggingService == null) throw new ArgumentException("LoggingService");
+            LoggingStorageFactory<ILog4NetLoggingService>.CreateStorageContainer().Store(loggingService);
+        }
+
+        private static void CreateLogger()
+        {
+            try
+            {
+                if (HttpContext.Current == null)
+                    DependencyResolver.Current.GetService<ThreadContextService>();
+                else
+                    DependencyResolver.Current.GetService<HttpContextProvider>();
+
+                var configuration = DependencyResolver.Current.GetService<IApplicationConfiguration>();
+
+                var loggingService = DependencyResolver.Current.GetService<ILog4NetLoggingService>();
+                var mapper = DependencyResolver.Current.GetService<IMapper>();
+                var addContextProvider = DependencyResolver.Current.GetService<IAddLoggingContextProvider>();
+                var wcfAppenderService = new Log4NetLoggingService(configuration, addContextProvider);
+
+                LoggingStorageFactory<ILog4NetLoggingService>.CreateStorageContainer().Store(loggingService);
+            }
+            catch (Exception logEx)
+            {
+                throw new Exception(
+                    "Exception while initializing and storing LoggingService instance into appropriate cache, detailed exc: " +
+                    logEx);
+            }
+        }
+
+        public static ILog4NetLoggingService GetLogger()
+        {
+            if (LoggingStorageFactory<ILog4NetLoggingService>.CreateStorageContainer().GetLogger() == null)
+                InitializeLogFactory(true);
+
+            return LoggingStorageFactory<ILog4NetLoggingService>.CreateStorageContainer().GetLogger();
+        }
+
+        public static ILog4NetLoggingService GetLoggerForDbInterceptor()
+        {
+            if (LoggingStorageFactory<ILog4NetLoggingService>.CreateStorageContainer().GetLogger() == null)
+                InitializeLogFactory(true);
+
+            return LoggingStorageFactory<ILog4NetLoggingService>.CreateStorageContainer().GetLogger();
+        }
     }
 }
