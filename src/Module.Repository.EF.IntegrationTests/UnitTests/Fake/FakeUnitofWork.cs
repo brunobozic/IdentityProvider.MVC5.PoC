112ceca846c42cae1279dea4012e4fd5d0432b98
Bruno Božić <bruno.bozic@gmail.com>
2024-03-03 18:47:53 +0100

    Needed to make it run, but its not the version I needed.

--- src/Module.Repository.EF.IntegrationTests/UnitTests/Fake/FakeUnitofWork.cs
+++ src/Module.Repository.EF.IntegrationTests/UnitTests/Fake/FakeUnitofWork.cs
@@ -6,33 +6,5 @@ using Module.Repository.EF.RowLevelSecurity;
 
 namespace Module.Repository.EF.IntegrationTests.UnitTests.Fake
 {
-    public class FakeUnitofWork : UnitOfWork
-    {
-        private readonly DbContext _context;
-
-        public FakeUnitofWork(DbContext context) : base(context , new RowAuthPoliciesContainer())
-        {
-            _context = context;
-        }
-
-        public override IRepositoryAsync<TEntity> RepositoryAsync<TEntity>()
-        {
-            if (Repositories == null)
-            {
-                Repositories = new Dictionary<string, dynamic>();
-            }
-
-            var type = typeof(TEntity).Name;
-
-            if (Repositories.ContainsKey(type))
-            {
-                return (IRepositoryAsync<TEntity>)Repositories[type];
-            }
-
-            // Add fake repository
-            var repositoryType = typeof(FakeRepository<>);
-            Repositories.Add(type, Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context, this));
-            return Repositories[type];
-        }
-    }
+  
 }
